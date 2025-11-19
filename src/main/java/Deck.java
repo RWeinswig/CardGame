edf0f95c06805ecd1cd60b2b062decf4bc5ae523
ryanweinswig <ryanweinswig@gmail.com>
2025-11-18 20:51:33 -0800

    Created files and completed Card class

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,2 +1,48 @@
+import java.util.ArrayList;
+
 public class Deck {
+    ArrayList<Card> cards;
+    int cardsLeft;
+
+    public Deck(ArrayList<Card> cards, String[] suits, int cardsLeft) {
+        cards = new ArrayList<Card>();
+    for (String suit: suits) {
+        for (int i=0; i< ranks.length; i++) {
+            cards.add(new Card(ranks[i], suit, values[i]));
+        }
+    }
+
+    cardsLeft = cards.size();
+    shuffle();
+    }
+
+    public boolean isEmpty() {
+        return cardsLeft == 0;
+    }
+
+    public int getCardsLeft() {
+        return cardsLeft;
+    }
+
+    public Card deal() {
+        if (isEmpty()) {
+            return null;
+        }
+        cardsLeft --;
+        return cards.get(cardsLeft);
+    }
+
+    public void shuffle() {
+        for (int i=cards.size() - 1; i>=0; i--) {
+            int random = (int)(Math.random() * (i+1));
+
+            // this is where we want to swap cards[i] and cards[random]
+            Card temp = cards.get(i);
+            cards.set(i, cards.get(random));
+            cards.set(random, temp);
+        }
+        // Want to reset the number of cards remaining back to the full deck
+        cardsLeft = cards.size();
+    }
+
 }
