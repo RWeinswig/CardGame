6450ac96463a1806b219ca6bea4ce499d933367e
ryanweinswig <ryanweinswig@gmail.com>
2025-11-19 13:58:47 -0800

    Worked on game, just put in basic parts of it

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class Game {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        playGame();
     }
+
+    public static void printInstructions() {
+        System.out.println("Triad Tactics is a 3-card strategy came where the player and computer draw hands and make choices each round. The player can spend energy to power up a card (by increasing its value by 2), swapping a card's suit, or pass to save energy for bonus points. After the player acts, the computer decides whether to improve its hand by swapping, powering up, or changing suits. Once both sides finish their actions, the hands are evaluated for combinations like three-of-a-kind, straights, flushes, or pairs, plus the extra bonuses. The higher-scoring hand wins the round and earns an additional point. After 5 rounds, the player with most total points win the game.");
+    }
+
+    public static void playGame() {
+        printInstructions();
+        System.out.println("Whenever you are ready to continue, hit enter: ");
+        String name = sc.nextLine();
+    }
+
+
+
 }
--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,2 +1,26 @@
+import java.util.ArrayList;
+
 public class Player {
+    ArrayList<Card> hand;
+    int points;
+    String name;
+
+    public Player(ArrayList<Card> hand, String name) {
+        this.hand = hand;
+        points = 0;
+        this.name = name;
+    }
+
+    public void addPoints(int pointsToAdd) {
+        points += pointsToAdd;
+    }
+
+    public void addCard(Card card) {
+        hand.add(card);
+    }
+
+    public String toString(){
+        return name + " has " + points + " points. \n " + name + "'s cards: " + hand;
+    }
+
 }
